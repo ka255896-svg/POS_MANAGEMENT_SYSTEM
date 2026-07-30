105e7e1783879ddbf83b8dadc1a2b89ab176da12
kapiku alex sam <ka255896@students.cavendish.ac.ug>
2026-07-30 12:48:38 -0700

    Migrated POS Management System from SQLite to MySQL

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "pos_management_system",
+        "USER": "root",
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
